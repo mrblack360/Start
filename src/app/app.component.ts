06d64113d7dc3c43f5dd23f1353c0a67df30f4e9
mrblack360 <rmaswi360@outlook.com>
2019-08-05 15:30:12 +0300

    Interface updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  tabs = ['Open Scientific Calculator', 'Help Others', 'Post Question', 'Profile'];
+  tabs = ['Scientific Calculator', 'Help Others', 'Post Question', 'Profile'];
   title = 'My HomeWorker';
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { task } from '../task';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
