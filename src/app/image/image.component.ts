6007b26f5c4d4452ec3e0b2b66682dea08899034
mrblack360 <rmaswi360@outlook.com>
2019-08-06 08:27:02 +0300

    image updates

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -6,15 +6,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./image.component.css']
 })
 export class ImageComponent implements OnInit {
-  
-  @Input('imageno') imgno;
-  
-  @Output() public imageEmitter: EventEmitter<string>=new EventEmitter();
 
   constructor() { }
-  
+
   ngOnInit() {}
-    fireEmitter(){
-      this.imageEmitter.emit("Do you also need help like this girl; here you are.  Photo By: Erick Chingalo");
-    }
-}
